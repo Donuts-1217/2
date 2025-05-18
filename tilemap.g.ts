708f99e08144c4b834920d90dc033c8c7ea1139f
Donuts-1217 <chianghansen0302@gmail.com>
2025-05-18 20:43:23 +0800

    MakeCode 專案的初始檔案

---
+++ tilemap.g.ts
@@ -0,0 +1,58 @@
+// 自動產生的程式碼。請勿編輯。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "層級1":
+            case "層級1":return tiles.createTilemap(hex`10001000010101010101010101010101010101010105060606060606060606060606040101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a090101070a0a0a0a0a0a0a0a0a0a0a0a09010102080808080808080808080808030101010101010101010101010101010101`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.castle.tilePath3,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tilePath6,sprites.castle.tilePath5], TileScale.Sixteen);
+            case "層級2":
+            case "層級2":return tiles.createTilemap(hex`100010000101010101010101010101010101010101030a0a0a0a0a0a0a0a0a0a0a0a09010104020202020202020202020202080101040202020202020202020202020801010402020202020202020202020208010104020202020202020202020202080101040202020202020202020202020801010402020202020202020202020208010104020202020202020202020202080101040202020202020202020202020801010402020202020202020202020208010104020202020202020202020202080101040202020202020202020202020801010402020202020202020202020208010105060606060606060606060606070101010101010101010101010101010101`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.forestTiles12,sprites.builtin.forestTiles10,sprites.builtin.forestTiles5,sprites.builtin.forestTiles9,sprites.builtin.forestTiles13,sprites.builtin.forestTiles14,sprites.builtin.forestTiles15,sprites.builtin.forestTiles11,sprites.builtin.forestTiles7,sprites.builtin.forestTiles6], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// 自動產生的程式碼。請勿編輯。
